dda4cc5bec20727bff5bd1b7d2a971ff5abb148d
Nick <37885459+NicolasBastos027@users.noreply.github.com>
2024-09-06 10:58:21 -0300

    Create anotacoes.cpp

---
+++ anotacoes.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    // pilha lista e fila ; (as 3 são iguais, a unica diferença é a disposição dos elementos)
+    // LISTA :
+    // lista são conjuntos de elementos, objetos, variaveis, tarefas ou qualquer coisa que possa enumerar e formar um conjunto. É igual uma lista de compras
+    
+    // listas encadeadas
+    // duplamente encadeadas - circulares
+    
+    // comportamento de uma lista
+    // fim de uma lista é o começo de outra
+    
+    // Filas
+    
+    //pilhas
+    // topo = null;
+    //pilhas podem ser implementadas em listas encadeadas ou vetores
+
+    return 0;
+}
